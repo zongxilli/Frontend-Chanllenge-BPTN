b9128a1fc9e166292eee7c6c1418ebbaced90c31
zongxilli <zongxi2014@gmail.com>
2021-10-20 00:37:48 -0400

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import Home from './screens/Home';
 import Liked from './screens/Liked';
 import Navbar from './components/Navbar';
