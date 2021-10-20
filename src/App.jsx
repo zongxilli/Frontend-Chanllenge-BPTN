c6b79c950a54cef8cea28aa9e082cb72ec3687ae
zongxilli <zongxi2014@gmail.com>
2021-10-19 22:44:16 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route } from 'react-router-dom';
+
+function App() {
+	return <div>hello</div>;
+}
+
+export default App;
---
+++ src/hooks/useVideos.jsx
@@ -0,0 +1,7 @@
+import React, { useEffect, useState } from 'react';
+
+const useVideos = () => {
+	return <div></div>;
+};
+
+export default useVideos;
